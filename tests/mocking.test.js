f427019e577d1669fcae1beee3d7ba12a84e0a38
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-29 18:51:52 +0530

    Exercise - 2: mocking values and interaction tests

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,12 +1,19 @@
 import { vi, it, expect, describe } from "vitest";
-import { getShippingInfo, renderPage } from "../src/mocking";
+import {
+	getShippingInfo,
+	renderPage,
+	submitOrder,
+} from "../src/mocking";
 import { getShippingQuote } from "../src/libs/shipping";
 import { trackPageView } from "../src/libs/analytics";
+import { charge } from "../src/libs/payment";
 
 // Mocking a 3rd party Module Values
 vi.mock("../src/libs/shipping");
 // Understanding the interaction between units
 vi.mock("../src/libs/analytics");
+// Exercise - 1
+vi.mock("../src/libs/payment");
 
 describe("Mock Function Basics", () => {
 	it("Definitions and Matchers", async () => {
@@ -61,5 +68,41 @@ describe("renderPage", () => {
 	it("should call analytics (interaction testing)", async () => {
 		await renderPage();
 		expect(trackPageView).toHaveBeenCalled();
+	});
+});
+
+describe("submitOrder", () => {
+	// Testing Interaction with other units
+	it("should charge the customer", async () => {
+		const order = { totalAmount: 35 };
+		const creditCard = { creditCardNumber: "3567 9876 3452" };
+		vi.mocked(charge).mockResolvedValue({ status: "success" });
+		await submitOrder(order, creditCard);
+		// Testing interaction with charge()
+		expect(charge).toHaveBeenCalledWith(
+			creditCard,
+			order.totalAmount
+		);
+	});
+
+	// Mocking charge() to test current unit behaviour
+	it("should return success if payment successful", async () => {
+		const order = { totalAmount: 35 };
+		const creditCard = { creditCardNumber: "3567 9876 3452" };
+		vi.mocked(charge).mockResolvedValue({ status: "success" });
+		const result = await submitOrder(order, creditCard);
+		expect(result).toEqual({ success: true });
+	});
+
+	// Mocking charge() to test current unit behaviour
+	it("should return error if payment unsuccessful", async () => {
+		const order = { totalAmount: 35 };
+		const creditCard = { creditCardNumber: "3567 9876 3452" };
+		vi.mocked(charge).mockResolvedValue({ status: "failed" });
+		const result = await submitOrder(order, creditCard);
+		expect(result).toHaveProperty("success");
+		expect(result).toHaveProperty("error");
+		expect(result.success).toBe(false);
+		expect(result.error).toBeTruthy();
 	});
 });
