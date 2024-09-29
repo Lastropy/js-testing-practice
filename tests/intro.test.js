7ede719a7898b14157baaf6b4e58fbc3034021a6
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-29 12:39:01 +0530

    Exercise : Added tests for FizzBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,6 +1,6 @@
 // The file to test intro.js
 import { describe, it, expect, assert } from "vitest";
-import { max } from "../src/intro";
+import { max, fizzBuzz } from "../src/intro";
 
 //describe - define a SUITE of tests
 describe("max", () => {
@@ -42,3 +42,22 @@ describe("max", () => {
 
 // After defining these tests, one can refactor their code
 // easily and see if the tests are still passing
+
+// Tests for fizzBuzz function
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if argument is divisible by both 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if argument is divisible by 3 only", () => {
+    expect(fizzBuzz(9)).toBe("Fizz");
+  });
+
+  it("should return Buzz if argument is divisible by 5 only", () => {
+    expect(fizzBuzz(25)).toBe("Buzz");
+  });
+
+  it("should return the number as string if argument is neither divisible by 3 nor 5", () => {
+    expect(fizzBuzz(23)).toBe("23");
+  });
+});
