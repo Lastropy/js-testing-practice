64f90d79a67d5c1941b18858d4ed5f7775568d2f
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-29 18:05:12 +0530

    Mock Function Assertion Matchers

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -3,9 +3,18 @@ import { vi, it, expect, describe } from "vitest";
 describe("test suite", () => {
 	it("test case", async () => {
 		const greet = vi.fn();
+
+		// Ways to mock a function
 		greet.mockReturnValue("hi"); // hi
 		greet.mockResolvedValue("Hello"); //  Promise { 'Hello' }
 		greet.mockImplementation((name) => "Hello " + name); // Hello Shivam
 		greet.mockImplementation(async (name) => "Hello " + name); //  Promise { 'Hello Shivam' }
+
+		// Mock Function Assertion Matchers
+		greet("Hi");
+		expect(greet).toHaveBeenCalled();
+		expect(greet).toHaveBeenCalledWith("Hi");
+		expect(greet).toHaveBeenCalledOnce();
+		expect(greet).toHaveBeenCalledTimes(1);
 	});
 });
